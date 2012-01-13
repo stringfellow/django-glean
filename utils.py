c39bd5fb6e99b4dfad53147a8e51a5b629693d06
Steve Pike <spike@maplecroft.com>
2012-01-13 12:55:09 +0000

    clean up utils and stub for idea

--- utils.py
+++ utils.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-15 -*-
-import getpass
-
-import galerts
 
 
-def alerts_manager():
-    un = raw_input("Email: ")
-    pwd = getpass.getpass()
-    gam = galerts.GAlertsManager(un, pwd)
-    pwd = None  # discard ASAP.
-    import pdb; pdb.set_trace()
+
+def paged_article_follower(url):
+    """For articles that are split, knows to follow the page links & join."""
+    pass
